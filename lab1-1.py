8840967b74536426ac5988d9e26ab117393d6798
Odysseas Kopakakis <od.kopakakis@gmail.com>
2023-03-02 20:23:42 +0200

    final version

--- lab1-1.py
+++ lab1-1.py
@@ -12,12 +12,11 @@ with requests.get(url) as response:
         print("This page uses cookies")
         for cookie in response.cookies:
             print("name of cookie: ", cookie.name)
-            if cookie.expires is not None:
-                cookie_expiration_time = datetime.datetime.fromtimestamp(cookie.expires) 
-            if cookie_expiration_time:
-                print("Cookie expiration time:", cookie_expiration_time)
+            if cookie.expires :
+                cookie_expiration_time = datetime.datetime.fromtimestamp(cookie.expires) - datetime.datetime.now()
+                print("Cookie will be valid for :", cookie_expiration_time)
             else:
-                print("No cookies with expiration time found")
+                print("No  expiration time found")
     else:
         print("This page does not use cookies")
 
